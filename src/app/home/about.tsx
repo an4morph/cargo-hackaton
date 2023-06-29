5394a1a35967129ee61ac7dc4c8054cbecb83761
an4morph <avgustin456@gmail.com>
2023-06-29 21:29:51 +0600

    fix home page

--- src/app/home/about.tsx
+++ src/app/home/about.tsx
@@ -1,6 +1,6 @@
 export const About = (): JSX.Element => {
   return (
-    <div className="bg-orange-200 h-[60vh]">
+    <div className="bg-orange-200">
       <div className="responsive py-10">
         <h2 className="mb-10 text-xl">Текст о компании и о ее функциях</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati vitae deleniti neque eos nam quisquam commodi possimus. Ut saepe, provident dicta quidem distinctio mollitia ea itaque nobis minima voluptates, sunt nulla tenetur laudantium eius doloribus pariatur odio earum officia ipsam cupiditate? Reprehenderit, exercitationem. Quibusdam sit reiciendis, quasi ab iste numquam qui quas autem ullam rerum! Ratione laudantium illum, cumque pariatur iusto doloribus odit sunt quaerat vel magni libero quos minus, similique accusantium eligendi, tenetur repellendus deserunt maiores dolores! Nihil, sit quae architecto amet dignissimos quo laborum minima? Ipsa aliquid maiores officia earum, iusto non voluptatem expedita. Delectus libero corporis nam.</p>
--- src/app/home/hero.tsx
+++ src/app/home/hero.tsx
@@ -1,8 +1,19 @@
+import { PrimaryButton } from '@/components/button/primary'
+import Link from 'next/link'
+
 export const Hero = (): JSX.Element => {
   return (
     <div className="h-[calc(100vh_-_56px)] bg-sky-200">
-      <div className="responsive">
-        Hero
+      <div className="responsive flex justify-between items-center h-full">
+        <div className="w-1/2 h-[300px] bg-slate-700 text-white flex justify-center items-center">тут картинка например</div>
+
+        <div>
+          <h1 className="text-3xl">Lorem ipsum dolor sit amet.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia eius ut est dignissimos veritatis, quis vero sapiente sint, officia eligendi at natus sunt accusamus!</p>
+          <Link href="/login">
+            <PrimaryButton>Начать работу</PrimaryButton>
+          </Link>
+        </div>
       </div>
     </div>
   )
