d460bb77bdabffd3dd28367166f1c8ed49aa4998
an4morph <avgustin456@gmail.com>
2023-07-04 12:28:52 +0600

    add login api changes

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,8 +1,8 @@
-export const API_URL = 'http://34.89.184.248/api'
+export const API_URL = 'http://34.89.184.248/api/v1'
 
 const url = (route: string) => `${API_URL}${route}`
 
-export const login = (body: { email: string, password: string }) => fetch(url('/account/login'), {
+export const login = (body: { email: string, password: string }) => fetch(url('/account/login/'), {
   method: 'POST',
   body: JSON.stringify(body),
   headers: {
