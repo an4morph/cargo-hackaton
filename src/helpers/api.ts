5810b7b5744ba81b2817ca99dc98fd444e481ccf
an4morph <avgustin456@gmail.com>
2023-07-04 13:54:18 +0600

    add profile reqs

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -2,6 +2,12 @@ export const API_URL = 'http://34.89.184.248/api/v1'
 
 const url = (route: string) => `${API_URL}${route}`
 
+const simpleGet = (route: string) => fetch(url(route))
+  .then((res) => {
+    if (!res.ok) throw new Error('Login fetch error')
+    return res.json()
+  })
+
 export const login = (body: { email: string, password: string }) => fetch(url('/account/login/'), {
   method: 'POST',
   body: JSON.stringify(body),
@@ -12,3 +18,7 @@ export const login = (body: { email: string, password: string }) => fetch(url('/
   if (!res.ok) throw new Error('Login fetch error')
   return res.json()
 })
+
+export const getDriverProfile = (id: string | number) => simpleGet(`/profile/driver/${id}`)
+export const getCompanyProfile = (id: string | number) => simpleGet(`/profile/company/${id}`)
+export const getShipperProfile = (id: string | number) => simpleGet(`/profile/shipper/${id}`)
