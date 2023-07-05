0336334ae363e275fe1b9e7d0cefb03928b53a7d
tilekturumkulov <turumkuloff@gmail.com>
2023-07-05 14:05:19 +0600

    add job model

--- src/types/models.d.ts
+++ src/types/models.d.ts
@@ -17,3 +17,24 @@ export interface ShipperModel {
   last_name: string
   image: string
 }
+
+export interface JobModel {
+  id: string | number
+  owner: string
+  title: string
+  pickup_location:string
+  pickup_date: string
+  delivery_date: string
+  destination_location:string
+  weight_of_goods: string
+  type_of_goods: string
+  required_equipment:string
+  special_instruction:string
+  driver_id:string | number
+  status: string
+  activation_code:string
+  complete_code: string
+  cancel_code:string
+  started_at:string
+  is_confirm:boolean
+}
