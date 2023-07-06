81dc3a909b849e744f15a50af1ed207f3d011d82
an4morph <avgustin456@gmail.com>
2023-07-06 17:46:46 +0600

    shipment structure

--- src/app/shipments/[id]/components/content.tsx
+++ src/app/shipments/[id]/components/content.tsx
@@ -45,43 +45,35 @@ export const ShimpmentPageContent = ({ data }: IProps): JSX.Element => {
           </p>
         </div>
 
-        <div className="w-1/5 min-w-[20%] paper p-4 ml-6">
-          <h2 className="text-xl mb-4">Your Carrier</h2>
-          <div className="w-full aspect-square rounded-full relative overflow-hidden">
-            <Image
-              fill
-              src="https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=989&q=80"
-              alt="driver"
-              className="object-cover"
-            />
-          </div>
-
-          <div className="mb-1 mt-2">
-            <span className="font-bold">Name:</span>
-      &nbsp;Steve Clark
-          </div>
-          <div className="mb-1">
-            <span className="font-bold">Cargo:</span>
-      &nbsp;Super Delivery
-          </div>
-          <p>Lorem, ipsum dolor sit amet Magni voluptate quod vel! Provident, molestias ipsa?</p>
-          <Link className="mt-4 block" href="/profile/driver/1">
-            <PrimaryButton className="w-full">More Info</PrimaryButton>
-          </Link>
-          <Link className="mt-4 block" href="/chat/1">
-            <SecondaryButton className="w-full">Message</SecondaryButton>
-          </Link>
+        <div className="w-1/5 min-w-[20%] paper p-4 ml-6 self-start">
+          <h2 className="text-lg font-bold mb-4">Driver block</h2>
+          {
+            data.driver_id
+              ? (
+                <Link href={`/driver/profile/${data.driver_id}`}>
+                  <PrimaryButton>Show driver profile</PrimaryButton>
+                </Link>
+              ) : (
+                <p className="text-slate-500">No driver found for this order yet</p>
+              )
+          }
         </div>
       </div>
 
 
-      <h2 className="text-xl mb-4 mt-10">Tracking</h2>
-      <div className="w-full h-[400px] relative">
-        <iframe
-          title="result"
-          src="http://34.89.184.248/api/v1/job/map/"
-        />
-      </div>
+      {
+        data.driver_id && (
+          <div>
+            <h2 className="text-xl mb-4 mt-10">Tracking</h2>
+            <div className="w-full h-[400px] relative">
+              <iframe
+                title="result"
+                src="http://34.89.184.248/api/v1/job/map/"
+              />
+            </div>
+          </div>
+        )
+      }
     </>
   )
 }
